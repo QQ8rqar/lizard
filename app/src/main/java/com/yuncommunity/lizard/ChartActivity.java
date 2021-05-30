4e3bbdce00950e9e4552c2f3eaea3ac7153fc2e5
QQ8rqar <dtf8159938425@163.com>
2021-05-30 19:50:36 +0800

    update

---
+++ app/src/main/java/com/yuncommunity/lizard/ChartActivity.java
@@ -0,0 +1,16 @@
+package com.yuncommunity.lizard;
+
+import android.os.Bundle;
+
+import com.yuncommunity.lizard.base.MyActivity;
+
+/**
+ * Created by oldfeel on 16-10-17.
+ */
+public class ChartActivity extends MyActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart_activity);
+    }
+}
--- app/src/main/java/com/yuncommunity/lizard/MainActivity.java
+++ app/src/main/java/com/yuncommunity/lizard/MainActivity.java
@@ -1,17 +1,49 @@
 package com.yuncommunity.lizard;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.pgyersdk.update.PgyUpdateManager;
 import com.yuncommunity.lizard.base.MyActivity;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends MyActivity {
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         PgyUpdateManager.register(this);
+    }
+
+    @OnClick(R.id.fab)
+    public void fab() {
+        openActivity(AddActivity.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_chart:
+                openActivity(ChartActivity.class);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
